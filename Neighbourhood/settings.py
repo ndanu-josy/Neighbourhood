16f96fbd2268f67664d3091d3a4ce24911442882
ndanu-josy <josphinendanu3@gmail.com>
2021-07-23 21:30:54 +0300

    Add secret key in env

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@vvyi0(dfs@b!bp6vt5z#mp-an^z44!&bpa7c7!o%4@8r^37o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
