346399784d9598f2e8de316a9366e400c2bb6f6b
ndanu-josy <josphinendanu3@gmail.com>
2021-07-23 22:32:19 +0300

    Add registartion form and the view function to display the html

--- neighapp/views.py
+++ neighapp/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth import login
+from neighapp.forms import RegistrationForm
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        
+        if form.is_valid():
+        
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+    return render(request, 'registration/registration_form.html', {"form":form}) 
