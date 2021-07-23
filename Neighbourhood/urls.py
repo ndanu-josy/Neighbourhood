31be6e16b820fe3bea59e2d2ae56cd780d93c9e6
ndanu-josy <josphinendanu3@gmail.com>
2021-07-23 21:55:58 +0300

    Include app urls in the project urls

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path    
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('neighapp.urls')),
+    path(r'^accounts/', include('registration.backends.simple.urls')),
+    path(r'^api-token-auth/', obtain_auth_token),
+    
 ]
