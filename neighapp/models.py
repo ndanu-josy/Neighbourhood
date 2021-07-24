8a591b78300a311650bbd53d459352554b21d4d1
ndanu-josy <josphinendanu3@gmail.com>
2021-07-24 10:40:44 +0300

    Create Neighbourhood model

--- neighapp/models.py
+++ neighapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    neighbourhood_name= models.CharField(max_length=80)
+    neighbourhood_location = models.CharField(max_length=80)
+    occupants_count = models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='adminstrator')
